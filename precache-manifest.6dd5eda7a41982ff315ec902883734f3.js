984f4bb90279e70a2c744628a63fa7b069073b5d
Shaikot007 <shaikot999@gmail.com>
2020-09-18 20:28:27 +0600

    Updates

---
+++ precache-manifest.6dd5eda7a41982ff315ec902883734f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a67bbd513cf5f0013178ca2eeaaf148b",
+    "url": "/Javascript-calculator/index.html"
+  },
+  {
+    "revision": "10ea5b90e21bb805693b",
+    "url": "/Javascript-calculator/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e79fab0067a90339d31e",
+    "url": "/Javascript-calculator/static/css/main.8c8e77f2.chunk.css"
+  },
+  {
+    "revision": "10ea5b90e21bb805693b",
+    "url": "/Javascript-calculator/static/js/2.12d5a6ad.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Javascript-calculator/static/js/2.12d5a6ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e79fab0067a90339d31e",
+    "url": "/Javascript-calculator/static/js/main.70df9934.chunk.js"
+  },
+  {
+    "revision": "fc45707f872a82dc3906",
+    "url": "/Javascript-calculator/static/js/runtime-main.ff2d941c.js"
+  }
+]);
